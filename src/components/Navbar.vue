066397322d3f1d7342403800a3bc8111d96c4ca0
Nicholas Roy <nicholas.roy12@hotmail.co.uk>
2020-04-25 19:24:13 +0100

    Adding link to logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,13 @@
 <template>
   <v-app-bar app dark color="#002272" class="navbar">
     <v-container>
+      <a href="/" class="heading-link"> 
       <div class="d-flex">
         <h1>coin</h1>
         <h1 class="sumption">sumption</h1>
         <v-icon large class="logo">mdi-currency-usd-circle-outline</v-icon>
       </div>
+      </a>
     </v-container>
   </v-app-bar>
 </template>
@@ -17,6 +19,9 @@ export default {
 </script>
 
 <style scoped>
+.heading-link {
+  text-decoration: none;
+}
 .sumption {
   color: #b5e0e8;
 }
